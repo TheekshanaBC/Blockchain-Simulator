69a2049609610253f9f8ffc5b1dcb7c346c665f1
TheekshanaBC <tishen.a@botcalm.com>
2026-07-06 12:01:21 +0530

    implement hashing logic with corresponding tests to prove deterministic behaviour of hashing function

--- internal/block/block.go
+++ internal/block/block.go
@@ -1,6 +1,12 @@
 package block
 
-import "time"
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"strings"
+	"time"
+)
 
 type Block struct {
 	Height       int
@@ -14,12 +20,22 @@ type Block struct {
 const GenesisPrevHash = "0000000000000000000000000000000000000000000000000000000000000000"
 
 func NewGenesisBlock() *Block {
-	return &Block{
+	block := &Block{
 		Height:       0,
 		Timestamp:    time.Now().Unix(),
 		Transactions: []string{"Genesis Block"},
 		PrevHash:     GenesisPrevHash,
 		Nonce:        0,
-		Hash:         "",
 	}
+	block.Hash = block.CalculateHash()
+	return block
+}
+
+func (b *Block) CalculateHash() string {
+	txData := strings.Join(b.Transactions, "") // In real project Merkle tree data structure can be used
+	record := fmt.Sprintf("%d%d%s%s%d", b.Height, b.Timestamp, txData, b.PrevHash, b.Nonce)
+	h := sha256.New()
+	h.Write([]byte(record))
+	hashedBytes := h.Sum(nil)
+	return hex.EncodeToString(hashedBytes)
 }
--- internal/block/block_test.go
+++ internal/block/block_test.go
@@ -12,5 +12,30 @@ func TestNewGenesisBlock(t *testing.T) {
 	if block.PrevHash != GenesisPrevHash {
 		t.Errorf("Expected PrevHash %s, got %s", GenesisPrevHash, block.PrevHash)
 	}
+}
 
+func TestCalculateHash(t *testing.T) {
+	block := &Block{
+		Height:       1,
+		Timestamp:    1720211552,
+		Transactions: []string{"Alice pays Bob 10 USD"},
+		PrevHash:     GenesisPrevHash,
+		Nonce:        12345,
+	}
+
+	hash1 := block.CalculateHash()
+
+	block.Transactions = []string{"Alice pays Bob 100 USD"}
+	hash2 := block.CalculateHash()
+
+	block.Transactions = []string{"Alice pays Bob 10 USD"}
+	hash3 := block.CalculateHash()
+
+	if hash1 == hash2 {
+		t.Errorf("Hashes should be different for different Transactions")
+	}
+
+	if hash1 != hash3 {
+		t.Errorf("Hashes should be same for same Transactions")
+	}
 }
